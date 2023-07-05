8fe94143d930fdc2a47b137acd9c58502f4e1469
winxtxr <107090196+winxtxr@users.noreply.github.com>
2023-07-05 11:45:30 +0700

    update

--- member_insert.php
+++ member_insert.php
@@ -3,7 +3,7 @@
 include "connect.php";
 
 $sql = "INSERT INTO tbl_member(firstname, lastname, email, phone, photo, address)
-VALUES ('$_POST[fisrtname]','$_POST[lastname]','$_POST[email]','$_POST[phone]','$_POST[photo]','$_POST[adress]')";
+VALUES ('$_POST[firstname]','$_POST[lastname]','$_POST[email]','$_POST[phone]','$_POST[photo]','$_POST[adress]')";
 
 if (mysqli_query($conn, $sql)) {
   echo "New record created successfully";
