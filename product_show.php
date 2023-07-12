c7c832e22af166b124ac9a323b1800e56100b494
winxtxr <107090196+winxtxr@users.noreply.github.com>
2023-07-12 15:37:14 +0700

    Update

--- product_show.php
+++ product_show.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" href="style.css"/>
 <?php
-
 include "connect.php";
 
 $sql = "SELECT * FROM tbl_product";
@@ -8,40 +7,36 @@ $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
-
   ?>
-  <table class="tb">
-    <th>ชื่อ</th>
-    <th>ราคา</th>
-    <th>จำนวนของ</th>
-    <th>คำอธิบาย</th>
-    <th>Edit</th>
-    <th>Delete</th>
-  
+  <table class="table">
+    <th> name </th>
+    <th> price </th>
+    <th> stock </th>
+    <th> description </th>
+    <th> edit </th>
+    <th> delete </th>
+
+
+    <?php
+    while ($row = mysqli_fetch_assoc($result)) {
+      ?>
+
+      <tr>
+        <td><?php echo"$row[name]"; ?> </td>
+        <td><?php echo"$row[price]"; ?> </td>
+        <td><?php echo"$row[stock]"; ?> </td>
+        <td><?php echo"$row[description]"; ?> </td>
+        <td> <?php echo"<a href ='product_edit.php?id=$row[id]'> edit </a>";?> </td>
+        <td> <?php echo"<a href ='product_del.php?id=$row[id]'> delete </a>";?> </td>
+    </tr>
+    <?php
+    }
+    ?>
+  </table>
+
   <?php
-
-  while($row = mysqli_fetch_assoc($result)) {
-   
-  ?>
-<tr>
-    <td><?php echo"$row[name]"; ?> </td>
-    <td><?php echo"$row[price]"; ?> </td>
-    <td><?php echo"$row[stock]"; ?> </td>
-    <td><?php echo"$row[description]"; ?> </td>
-    <td><?php echo"<a href='product_edit.php?id=$row[id]'> Edit </a>";?> </td>
-    <td><?php echo"<a href='product_del.php?id=$row[id]'> Delete </a>";?> </td>
-  </tr>
-
-<?php
-  }
-?>
-
-</table>
-
-<?php
 } else {
   echo "0 results";
 }
-
 mysqli_close($conn);
 ?>
